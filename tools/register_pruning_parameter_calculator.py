a1ee592a16ac3fd637f20777bdcbcc2829a00d8f
Benjamin Menkuec <benjamin@menkuec.de>
2021-02-16 20:44:55 +0100

    added script for calculation of pruning paramter of CIC

---
+++ tools/register_pruning_parameter_calculator.py
@@ -0,0 +1,17 @@
+from calculate_register_pruning import *
+import timeit
+
+N=6
+R=4095
+M=1
+start = timeit.default_timer()
+B_j = calculate_register_pruning(R=R, N=N, M=M, INP_DW=32, OUT_DW=32)
+end = timeit.default_timer()
+print(F"elapsed time {end - start} s")
+print(B_j)
+
+ret = 0
+for i in range(1,2*N+2):
+    print(f"B_j[{i}] = {B_j[i]}")
+    ret += int(B_j[i])<<(32*(i))
+print(F"pruning parameter = {ret}")
