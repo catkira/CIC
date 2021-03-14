696a4970217e04e34771df0e63651add932c7a19
Benjamin Menkuec <benjamin@menkuec.de>
2021-03-14 11:45:53 +0100

    added out_samp_str to integrator

--- hdl/integrator.sv
+++ hdl/integrator.sv
@@ -8,17 +8,22 @@ module integrator
     input                                       reset_n,
     input wire signed   [DATA_WIDTH_INP - 1:0]  inp_samp_data,
     input                                       inp_samp_str,
-    output wire signed  [DATA_WIDTH_OUT - 1:0]  out_samp_data
+    output wire signed  [DATA_WIDTH_OUT - 1:0]  out_samp_data,
+    output reg                                  out_samp_str
 );
 /*********************************************************************************************/
 localparam SUMMER_WIDTH = DATA_WIDTH_INP > DATA_WIDTH_OUT ? DATA_WIDTH_INP : DATA_WIDTH_OUT;
-wire    signed  [SUMMER_WIDTH - 1:0]    sum;
 reg     signed  [SUMMER_WIDTH - 1:0]    acc_reg;
-assign #4       sum = acc_reg + inp_samp_data;  // delay for 18x18 multiplier of Cyclone V SE is 3.4 ns
-always @(posedge clk)
+always_ff @(posedge clk)
 begin
-    if              (!reset_n)      acc_reg <= '0;
-    else    if      (inp_samp_str)  acc_reg <= sum;
+    if (!reset_n) begin
+        acc_reg <= 0;
+        out_samp_str <= 0;
+    end 
+    else begin
+        acc_reg <= inp_samp_str ? acc_reg + inp_samp_data : acc_reg;
+        out_samp_str <= inp_samp_str;
+    end
 end
 
 assign out_samp_data = acc_reg[SUMMER_WIDTH - 1 -: DATA_WIDTH_OUT];
