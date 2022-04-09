6687fa8ba60cb51f6843d9dd7511080be800114f
Benjamin Menkuec <benjamin@menkuec.de>
2022-04-09 13:47:51 +0200

    remove unused import

--- tests/test_cic_d.py
+++ tests/test_cic_d.py
@@ -2,7 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge, ReadOnly
-from fixedpoint import FixedPoint
 from collections import deque
 
 import random
